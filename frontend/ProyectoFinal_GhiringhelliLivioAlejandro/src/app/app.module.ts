6e5cd5b5c34125dd7929fba28ccd7ce6a9e88afd
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2022-08-16 23:08:19 -0300

    agregando componentes al front

--- frontend/ProyectoFinal_GhiringhelliLivioAlejandro/src/app/app.module.ts
+++ frontend/ProyectoFinal_GhiringhelliLivioAlejandro/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedessocialesComponent } from './componentes/redessociales/redessociales.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedessocialesComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
