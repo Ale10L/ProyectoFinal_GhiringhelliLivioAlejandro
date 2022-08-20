842b4487a628e6bc4ac6fb92551497618a4833df
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2022-08-20 11:13:02 -0300

    creacion de proyecto java spring boot

---
+++ backend/alejandro/src/main/java/com/portfolio/alejandro/AlejandroApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.alejandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlejandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlejandroApplication.class, args);
+	}
+
+}
