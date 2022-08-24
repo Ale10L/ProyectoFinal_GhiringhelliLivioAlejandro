0ca810adc6809049a6093f1080dfec90171e9ae4
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2022-08-23 23:44:17 -0300

    arreglos front - componentes home y login realizados

--- frontend/ProyectoFinal_GhiringhelliLivioAlejandro/src/app/componentes/redessociales/redessociales.component.ts
+++ frontend/ProyectoFinal_GhiringhelliLivioAlejandro/src/app/componentes/redessociales/redessociales.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-redessociales',
@@ -9,10 +10,13 @@ import { PersonaService } from 'src/app/service/persona.service';
 })
 export class RedessocialesComponent implements OnInit {
   persona: Persona = new Persona("", "", "", "", "", "", "", "", "", "", "", "");
-  constructor(public personaService: PersonaService) { }
+  constructor(public personaService: PersonaService, private router: Router) { }
 
   ngOnInit(): void {
     this.personaService.getPersona().subscribe(data => {this.persona = data});
   }
 
+  login(){
+    this.router.navigate(['/login']);
+  }
 }
